bb016cbcda5af5180f98c01d8af2353c0102e7a3
cits0907 <mac@mac.local>
2020-11-22 18:15:33 +0900

    Make Changeable the characters to use

--- rpg/script.js
+++ rpg/script.js
@@ -1,19 +1,21 @@
-function createPassword(length, useNumbers, useLowers, useUppers, useSymbols) {
-	var letters = "";
-	letters += useNumbers ? "0123456789" : "";
-	letters += useLowers ? "abcdefghijklmnopqrstuvwxyz" : "";
-	letters += useUppers ? "ABCDEFGHIJKLMNOPQRSTUVWXYZ" : "";
-	letters += useSymbols ? "!\"#$%&'()-=^~\\|@`[{;+:*]},<.>/?_" : "";
+function createPassword(length, characters) {
+	var result = "";
+	var i;
+	for (i = 0; i < characters.length; i++) {
+		if(result.indexOf(characters.charAt(i)) == -1) {
+			result += characters.charAt(i);
+		}
+	}
 
-	if (letters == "" || length <= 0) {
+	if (result.length < 2 || length < 1) {
 		return "Error";
 	}
 
-	var i = 0, password = "";
+	i = 0, password = "";
 	while (i < length) {
-		let random = Math.floor(Math.random() * letters.length);
-		if (password.slice(-1) != letters.charAt(random)) {
-			password += letters.charAt(random);
+		let random = Math.floor(Math.random() * result.length);
+		if (password.slice(-1) != result.charAt(random)) {
+			password += result.charAt(random);
 			i++;
 		}
 	}
@@ -21,13 +23,12 @@ function createPassword(length, useNumbers, useLowers, useUppers, useSymbols) {
 }
 
 function onGenerateButtonPressed() {
-	let passwordElement = document.getElementById("password");
-	let length = document.getElementById("length").value;
-	let useNumber = document.getElementById("useNumber").checked;
-	let useLower = document.getElementById("useLower").checked;
-	let useUpper = document.getElementById("useUpper").checked;
-	let useSymbol = document.getElementById("useSymbol").checked;
-	passwordElement.value = createPassword(length, useNumber, useLower, useUpper, useSymbol);
+	var characters = "";
+	characters += document.getElementById("useNumber").checked ? document.getElementById("includedNumber").value : "";
+	characters += document.getElementById("useLower").checked ? document.getElementById("includedLower").value : "";
+	characters += document.getElementById("useUpper").checked ? document.getElementById("includedUpper").value : "";
+	characters += document.getElementById("useSymbol").checked ? document.getElementById("includedSymbol").value : "";
+	document.getElementById("password").value = createPassword(document.getElementById("length").value, characters);
 }
 
 function onCopyButtonPressed() {
@@ -37,7 +38,16 @@ function onCopyButtonPressed() {
 	passwordElement.blur();
 }
 
-function onCheckboxTitleClicked(type){
-	let checkboxElement = document.getElementById(type);
+function onCheckboxTitleClicked(className) {
+	let checkboxElement = document.getElementsByClassName(className)[0];
 	checkboxElement.checked = checkboxElement.checked ? false : true;
+
+	onCheckBoxChanged(className);
+}
+
+function onCheckBoxChanged(className) {
+	let checkboxElement = document.getElementsByClassName(className)[0];
+	let charactersElement = document.getElementsByClassName(className)[1];
+
+	charactersElement.disabled = checkboxElement.checked ? false : true;
 }
