844319005bc69b8d399075abc60c428cdafea409
cits0907 <mac@mac.local>
2021-04-14 22:03:55 +0900

    Optimize HTML and CSS

--- rpg/script.js
+++ rpg/script.js
@@ -1,33 +1,41 @@
+// パスワードを作成する
 function createPassword(length, characters) {
-	var result = "";
+	var tmpDuplicates = "";
 	var i;
+	// 文字種の重複を削除
 	for (i = 0; i < characters.length; i++) {
-		if(result.indexOf(characters.charAt(i)) == -1) {
-			result += characters.charAt(i);
+		if(tmpDuplicates.indexOf(characters.charAt(i)) == -1) {
+			tmpDuplicates += characters.charAt(i);
 		}
 	}
+	characters = tmpDuplicates;
 
-	if (result.length < 2 || length < 1) {
-		return "Error";
+	// 重複削除の結果，1種類しか残らない場合，長さが1未満の場合は終了
+	if (characters.length < 2 || length < 1) {
+		alert("入力に誤りがあります");
+		return "エラー";
 	}
 
+	// 文字が連続しないようにしながらパスワードを生成
 	i = 0, password = "";
 	while (i < length) {
-		let random = Math.floor(Math.random() * result.length);
-		if (password.slice(-1) != result.charAt(random)) {
-			password += result.charAt(random);
+		let random = Math.floor(Math.random() * characters.length);
+		if (password.slice(-1) != characters.charAt(random)) {
+			password += characters.charAt(random);
 			i++;
 		}
 	}
 	return password;
 }
 
+// 作成ボタンが押された時の処理
 function onGenerateButtonPressed() {
 	var length = document.getElementById("length").value;
 	if (length >= 100000 && !confirm("処理に時間がかかりますがよろしいですか？")) {
 		return;
 	}
 
+	// 入力フォームの内容を取得し，createPasswordを呼ぶ
 	var characters = "";
 	characters += document.getElementById("useNumber").checked ? document.getElementById("includedNumber").value : "";
 	characters += document.getElementById("useLower").checked ? document.getElementById("includedLower").value : "";
@@ -36,6 +44,7 @@ function onGenerateButtonPressed() {
 	document.getElementById("password").value = createPassword(length, characters);
 }
 
+// コピーボタンが押された時の処理
 function onCopyButtonPressed() {
 	let passwordElement = document.getElementById("password");
 	passwordElement.select();
@@ -43,20 +52,25 @@ function onCopyButtonPressed() {
 	passwordElement.blur();
 }
 
+// チェックボックスに対応した文字をクリックされた時
 function onCheckboxTitleClicked(className) {
+	// チェックボックスを反転させる
 	let checkboxElement = document.getElementsByClassName(className)[0];
-	checkboxElement.checked = checkboxElement.checked ? false : true;
+	checkboxElement.checked = !checkboxElement.checked;
 
 	onCheckBoxChanged(className);
 }
 
+// チェックボックスがクリックされた時の処理
 function onCheckBoxChanged(className) {
 	let checkboxElement = document.getElementsByClassName(className)[0];
 	let charactersElement = document.getElementsByClassName(className)[1];
 
-	charactersElement.disabled = checkboxElement.checked ? false : true;
+	// 入力フォームを無効化
+	charactersElement.disabled = !checkboxElement.checked;
 }
 
+// 初期化ボタンが押された時の処理
 function onResetButtonPressed() {
 	document.getElementById("length").value = 16;
 	document.getElementById("password").value = "";
@@ -73,3 +87,6 @@ function onResetButtonPressed() {
 	document.getElementById("includedUpper").disabled = false;
 	document.getElementById("includedSymbol").disabled = true;
 }
+
+// 初回読み込み時，初期化する．
+onResetButtonPressed();
